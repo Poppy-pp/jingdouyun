e8ae9023b42f2eb9e0d9ffeb556b339efeb8e080
Pulin <15528842282@163.com>
2019-07-09 10:58:50 +0800

    7-9

---
+++ src/pages/findSite/index.vue
@@ -0,0 +1,21 @@
+<!-- 找场地-首页 -->
+<template>
+   <div class="">
+111
+   </div>
+</template>
+
+<script>
+export default {
+   data() {
+       return {}
+   },
+    components: {},
+    computed:{},
+    methods:{},
+    created(){}
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/pages/myCenter/index.vue
@@ -0,0 +1,20 @@
+<template>
+   <div class="">
+222
+   </div>
+</template>
+
+<script>
+export default {
+   data() {
+       return {}
+   },
+  components: {},
+  computed:{},
+  methods:{},
+  created(){}
+}
+</script>
+
+<style scoped>
+</style>
