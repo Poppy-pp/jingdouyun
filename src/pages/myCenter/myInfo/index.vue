78035ebc1c81a1eeb756ed854cdc26087ba3bef3
wpp <15528842282@163.com>
2019-08-04 12:37:23 +0800

    退出登录

--- src/pages/myCenter/myInfo/index.vue
+++ src/pages/myCenter/myInfo/index.vue
@@ -2,41 +2,46 @@
 <template>
    <div class="container">
       <van-field v-model="phone" label="手机号" readonly="true"  />
-      <button class="free-btn">退出登录</button>
+      <button class="free-btn" @click="loginOut">退出登录</button>
    </div>
 </template>
 
 <script>
 export default {
-   data() {
-       return {
-          phone:'184****4587'
-       }
-   },
+  data() {
+    return {
+      phone: "184****4587"
+    };
+  },
   components: {},
-  computed:{},
-  methods:{
+  computed: {},
+  methods: {
+    loginOut() {
+      wx.navigateTo({
+        url: "/pages/login/main"
+      });
+    }
   },
-  mounted () {
-  },
-  created(){}
-}
+  mounted() {},
+  created() {}
+};
 </script>
 
 <style lang="stylus" scoped>
-.container{
-   .free-btn{
-      background: linear-gradient(to right, #02d5fc 0%,#1fa5ff 100%);
-      width: 95%;
-      color: #fff;
-      padding: 6px 0;
-      margin-top: 15px;
-      border-radius: 3px;
-      font-size 14px
-   }
-   .free-btn::after{ 
-      border: none; 
-      outline: none;
-   }
+.container {
+    .free-btn {
+        background: linear-gradient(to right, #02d5fc 0%, #1fa5ff 100%);
+        width: 95%;
+        color: #fff;
+        padding: 6px 0;
+        margin-top: 15px;
+        border-radius: 3px;
+        font-size: 14px;
+    }
+
+    .free-btn::after {
+        border: none;
+        outline: none;
+    }
 }
 </style>
