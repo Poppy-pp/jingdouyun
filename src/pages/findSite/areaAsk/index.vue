d4b4bbc06165040a12be974a5e912f3196e23920
wpp <15528842282@163.com>
2019-08-11 18:09:31 +0800

    sdsd

--- src/pages/findSite/areaAsk/index.vue
+++ src/pages/findSite/areaAsk/index.vue
@@ -13,58 +13,6 @@
 
 <script>
 export default {
-<<<<<<< HEAD
-  data() {
-    return {
-      area: "",
-      areas: [
-        { text: "不限", children: [{ text: "不限", id: 1, disabled: false }] },
-        {
-          text: "附近",
-          children: [
-            { text: "不限", id: 2, disabled: false },
-            { text: "500m以内", id: 3, disabled: false },
-            { text: "1000m以内", id: 4, disabled: false }
-          ]
-        },
-        {
-          text: "区域",
-          children: [
-            { text: "不限", id: 5, disabled: false },
-            { text: "东城区", id: 6, disabled: false },
-            { text: "武侯区", id: 7, disabled: false }
-          ]
-        },
-        {
-          text: "地铁",
-          children: [
-            { text: "不限", id: 8, disabled: false },
-            { text: "1号线", id: 9, disabled: false },
-            { text: "3号线", id: 10, disabled: false }
-          ]
-        },
-        {
-          text: "道路",
-          children: [
-            { text: "不限", id: 12, disabled: false },
-            { text: "一环", id: 13, disabled: false },
-            { text: "四环", id: 14, disabled: false }
-          ]
-        },
-        {
-          text: "商圈",
-          children: [
-            { text: "总部基地", id: 15, disabled: false },
-            { text: "中关村", id: 16, disabled: false },
-            { text: "软件园", id: 17, disabled: false }
-          ]
-        }
-      ],
-      mainActiveIndex: 0, // 左侧高亮元素的index
-      activeId: 1 // 被选中元素的id
-    };
-  },
-=======
    data() {
        return {
           area:'',
@@ -81,7 +29,6 @@ export default {
           arr:[],
        }
    },
->>>>>>> 5a946e7b15f3b24ba39be291a7a1e9b89f223318
   components: {},
   computed: {},
   methods: {
@@ -92,15 +39,7 @@ export default {
     },
     // 点击子集
     onClickItem(e) {
-<<<<<<< HEAD
-      console.log(e);
-      this.activeId = e.mp.detail.id;
-      this.showPopup = false;
-      this.area = this.activeId;
-
-=======
       this.activeId = e.mp.detail.id;//选中变蓝色
->>>>>>> 5a946e7b15f3b24ba39be291a7a1e9b89f223318
       // 带参返回上一页
       var pages = getCurrentPages();
       var prevPage = pages[pages.length - 2]; //上一个页面
